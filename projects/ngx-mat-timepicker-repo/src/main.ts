3498644aab3243b8c46dc4f60a49225c8d28d81f
Kévin Dahmani <kevin-externe.dahmani@enedis.fr>
2025-11-27 09:35:37 +0100

    update dependencies to Angular 21

--- projects/ngx-mat-timepicker-repo/src/main.ts
+++ projects/ngx-mat-timepicker-repo/src/main.ts
@@ -1,8 +1,9 @@
+import { provideZoneChangeDetection } from "@angular/core";
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
 import { NgxMatTimepickerAppModule } from "./app/app.module";
 
 
 platformBrowserDynamic()
-    .bootstrapModule(NgxMatTimepickerAppModule)
+    .bootstrapModule(NgxMatTimepickerAppModule, { applicationProviders: [provideZoneChangeDetection()], })
     .catch(err => console.error(err));
